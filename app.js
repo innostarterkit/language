c98e73fdad1060181549b026765346e8dfa7f62b
innostarterkit <innostarterkit@gmail.com>
2019-05-29 11:07:20 +0200

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/language', function (req, res) {
 })
 
 app.get('/', function (req, res) {
-  res.send('Hello World !')
+  res.send('Hello World ! 2')
 })
 
 app.post('/', function (req, res) {
